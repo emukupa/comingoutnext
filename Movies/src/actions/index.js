aca7a1681bc2969d002cb365e2769cc9af02e9d1
Edward Manda <lilkups@yahoo.com>
2018-04-08 12:43:07 -0500

    worked on the todos

--- Movies/src/actions/index.js
+++ Movies/src/actions/index.js
@@ -1,26 +1,30 @@
 import axios from 'axios';
 
+//const URL = `https://jsonplaceholder.typicode.com/photos`;
+const OMDBAPI_URL = `http://www.omdbapi.com`;
+const API_KEY = `apikey=2f5c5c64`;
+const search = `batman`;
+const URL = `${OMDBAPI_URL}/?t=${search}&${API_KEY}`;
 
-const URL = `https://jsonplaceholder.typicode.com/photos`;
+console.log(URL);
 
-export const 
-    FETCHING    =   'FETCHING',
-    FETCHED     =   'FETCHED',
-    ERROR       =   'ERROR';
+export const FETCHING = 'FETCHING',
+  FETCHED = 'FETCHED',
+  ERROR = 'ERROR';
 
 export const getMovies = () => dispatch => {
-    dispatch({ type: FETCHING });
+  dispatch({ type: FETCHING });
 
-    axios   
-        .get(URL)
-        .then(response => response.data)
-        .then(data => {
-            const reducedData = data.filter(movie => movie.id < 10)
-            dispatch({ type: FETCHED, movies: reducedData })
-        })  
-        .catch(err => {
-            dispatch({ type: ERROR, movieError: 'Could not get any movies' })
-        });
-    };
-    
+  axios
+    .get(URL)
+    .then(response => response.data)
+    .then(data => {
+      const reducedData = data.filter(movie => movie.id < 10);
+      dispatch({ type: FETCHED, movies: reducedData });
+    })
+    .catch(err => {
+      dispatch({ type: ERROR, movieError: 'Could not get any movies' });
+    });
+};
+
 // dispatch({ type: FETCHED, movies: response.data })
--- Movies/src/todo.md
+++ Movies/src/todo.md
@@ -1,18 +1,29 @@
+# states to achieve
+
 STATE
-    topRatedMovies = [],
-    upcomingMovies = [],
-    nowPlayingMovies = [],
-    popularMovies = [],
-    popularTvShoes = [],
-    topRatedShows = [],
-    onTheAirTvShows= [],
-    airingTodayTvShows = [],
-    popularPeople = [],
+topRatedMovies = [],
+upcomingMovies = [],
+nowPlayingMovies = [],
+popularMovies = [],
+popularTvShoes = [],
+topRatedShows = [],
+onTheAirTvShows= [],
+airingTodayTvShows = [],
+popularPeople = [],
+
+# pagination/ results ?
 
 1 of 5000
 {
-    albumId: 1, 
-    id: 1, 
-    title: "accusamus beatae ad facilis cum similique qui sunt", url:"http://placehold.it/600/92c952", 
-    thumbnailUrl: "http://placehold.it/150/92c952"
+albumId: 1,
+id: 1,
+title: "accusamus beatae ad facilis cum similique qui sunt", url:"http://placehold.it/600/92c952",
+thumbnailUrl: "http://placehold.it/150/92c952"
 }
+
+# sign up for themoviedb.org
+
+#1. read documentation
+#2. play around with the datasets
+#3 note down ideas of how to proceed!
+#4 figure out how to ignore this file
